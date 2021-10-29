ea6e8e6f9aea1dfb27dbe62fc5090fa03928ebe4
rajib <rajib18197@gmail.com>
2021-10-29 14:26:05 +0600

    adding some functionality & UI-Components

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+/////////////////////////////////////////
+///////////////// Tabs Components
+const tabs = document.querySelectorAll('.side-nav__list');
+const tabContainer = document.querySelector('.side-nav__lists');
+const tabContent = document.querySelectorAll('.main-content');
 
+tabContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.side-nav__list');
+  // Guard Clause
+  if (!clicked) return;
+  tabs.forEach(tab => tab.classList.remove('side-nav__list--active'));
+  tabContent.forEach(t => t.classList.remove('main-content__active'));
+  document
+    .querySelector(`.side-nav__list-${clicked.dataset.tab}`)
+    .classList.add('side-nav__list--active');
+  document
+    .querySelector(`.main-content--${clicked.dataset.tab}`)
+    .classList.add('main-content__active');
+});
