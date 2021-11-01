215ab8fa694ff706604ec83998161ab4a6d81912
rajib <rajib18197@gmail.com>
2021-11-01 21:45:28 +0600

    design & functionality almost completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-/////////////////////////////////////////
-///////////////// Tabs Components
+///////////////////////////////////////////////////////////
+// Tabs Components [navigate home, question & tags page]
 const tabs = document.querySelectorAll('.side-nav__list');
 const tabContainer = document.querySelector('.side-nav__lists');
 const tabContent = document.querySelectorAll('.main-content');
@@ -8,6 +8,7 @@ tabContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.side-nav__list');
   // Guard Clause
   if (!clicked) return;
+
   tabs.forEach(tab => tab.classList.remove('side-nav__list--active'));
   tabContent.forEach(t => t.classList.remove('main-content__active'));
   document
@@ -17,7 +18,8 @@ tabContainer.addEventListener('click', function (e) {
     .querySelector(`.main-content--${clicked.dataset.tab}`)
     .classList.add('main-content__active');
 });
-// user profile
+/////////////////////////////////////////////////
+// User profile activity & Profile Settings
 const usertabs = document.querySelectorAll('.options__tab');
 const userTabContainer = document.querySelector('.options__tab-container');
 const userTabContent = document.querySelectorAll('.options__content');
@@ -25,9 +27,9 @@ const userTabContent = document.querySelectorAll('.options__content');
 userTabContainer.addEventListener('click', function (e) {
   const clicked1 = e.target.closest('.options__tab');
   console.log(clicked1);
-
+  // Guard Clause
   if (!clicked1) return;
-  //remove
+
   usertabs.forEach(tab => tab.classList.remove('options__tab--active'));
   userTabContent.forEach(content =>
     content.classList.remove('options__content--active')
@@ -40,11 +42,11 @@ userTabContainer.addEventListener('click', function (e) {
     .classList.add('options__content--active');
 });
 
-//
-const profile = document.querySelector('.navigation__profile');
+///////////////////////////////////////////
+// when User Clicks his/her profile
+const profile = document.querySelector('.navigation__profile-user');
 
 profile.addEventListener('click', function () {
-  //tabs.forEach(tabslist => tabslist.classList.remove('side-nav__list'));
   tabContent.forEach(content =>
     content.classList.remove('main-content__active')
   );
@@ -53,7 +55,8 @@ profile.addEventListener('click', function () {
     .classList.add('main-content__active');
 });
 
-// modal
+////////////////////////////////////////
+// modal window for asked questions
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn--close-modal');
@@ -61,14 +64,10 @@ const askQuestion = document.querySelector('.btn');
 
 const openModal = function (e) {
   e.preventDefault();
-  //modal.classList.add('show-modal');
-  //overlay.classList.add('open-overlay');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 const closeModal = function () {
-  //modal.classList.remove('show-modal');
-  //overlay.classList.remove('open-overlay');
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -77,10 +76,13 @@ btnClose.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  // if (e.key === 'Escape' && modal.classList.contains('show-modal')) {
-  //   closeModal();
-  // }
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+// for testing login form
+const btnSign = document.querySelector('.sign-in');
+btnSign.addEventListener('click', function (e) {
+  e.preventDefault();
+  document.querySelector('.section-book').classList.add('sign-in-form');
 });
